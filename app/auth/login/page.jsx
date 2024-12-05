620d2bcb9af01f5458d64e7242c777286d772d54
dalienst <oduordalienst@gmail.com>
2024-12-05 14:57:36 +0300

    reinstall next auth

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+"use client";
+
+import { getSession, signIn } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const response = await signIn("credentials", {
+      redirect: false,
+      email: email,
+      password: password,
+    });
+
+    if (response?.error) {
+      setLoading(false);
+      toast?.error("Invalid email or password");
+    } else {
+      toast?.success("Login successful! Redirecting...");
+      setLoading(false);
+      router.push("/dashboard");
+    }
+  };
   return (
-    <div>LoginPage</div>
-  )
+    <div className="container min-h-screen flex items-center justify-center"></div>
+  );
 }
