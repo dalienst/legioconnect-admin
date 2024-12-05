bf56d9339b743f5d0472f816324113f7715f25e9
dalienst <oduordalienst@gmail.com>
2024-12-05 14:21:46 +0300

    switch to bootstrap

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <title>LegioConnect</title>
+        <meta
+          name="description"
+          content="LegioConnect is an application created for members of Legio Maria and those interested in learning more about the faith. It offers rich content on Legio Maria’s history, prayers, and the Bible, for deeper engagement with the faith."
+        />
       </head>
       <body>{children}</body>
     </html>
--- app/page.jsx
+++ app/page.jsx
@@ -1,82 +1,95 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-"use client";
-import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-  const [bible, setBible] = useState([]);
-  const url = `https://api.scripture.api.bible/v1/bibles?language=eng&include-full-details=false`;
-
-  const fetchBible = async () => {
-    try {
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          "api-key": "ec154d957ebe133d3aa9840c7d951dc8",
-          "Content-Type": "application/json",
-        },
-      });
-
-      const data = await response.json();
-      setBible(data);
-    } catch (error) {
-      console.error("Error fetching Bibles:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchBible();
-  }, []);
-
-  if (!bible) return <div className="text-center mt-8">Loading...</div>;
-
   return (
-    <div className="min-h-screen py-8 px-4 sm:px-8 bg-gray-50">
-      <div className="max-w-4xl mx-auto bg-white shadow-md rounded-md p-6">
-        <h2 className="text-2xl font-semibold text-black mb-4">
-          Select Bible
-        </h2>
-        <div className="overflow-x-auto">
-          <table className="w-full border-collapse border border-gray-200">
-            <thead>
-              <tr className="bg-gray-100">
-                <th className="text-left px-4 py-2 border-b border-gray-200">
-                  Abbr
-                </th>
-                <th className="text-left px-4 py-2 border-b border-gray-200">
-                  Name
-                </th>
-                <th className="text-left px-4 py-2 border-b border-gray-200">
-                  Action
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {bible?.data?.map((bible) => (
-                <tr
-                  key={bible?.id}
-                  className="hover:bg-gray-50 transition duration-150 text-black"
-                >
-                  <td className="px-4 py-2 border-b border-gray-200">
-                    {bible?.abbreviationLocal}
-                  </td>
-                  <td className="px-4 py-2 border-b border-gray-200">
-                    {bible?.name}
-                  </td>
-                  <td className="px-4 py-2 border-b border-gray-200">
-                    <Link
-                      href={`/${bible?.id}`}
-                      className="text-blue-600 hover:text-blue-800 transition duration-150 underline"
-                    >
-                      Select
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol>
+          <li>
+            Get started by editing <code>app/page.js</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.logo}
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+            Read our docs
+          </a>
         </div>
-      </div>
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
