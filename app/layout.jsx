d8e4f75f66cf260e89ada494ca5ed2ad554774e3
dalienst <oduordalienst@gmail.com>
2024-12-05 14:37:09 +0300

    set up next auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,11 @@
+"use client";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
+import NextAuthProvider from "@/providers/NextAuthProvider";
+import TanstackQueryProvider from "@/providers/TanstackQueryProvider";
+import BootstrapClient from "@/providers/BootstrapClient";
 
 export default function RootLayout({ children }) {
   return (
@@ -10,7 +17,13 @@ export default function RootLayout({ children }) {
           content="LegioConnect is an application created for members of Legio Maria and those interested in learning more about the faith. It offers rich content on Legio Maria’s history, prayers, and the Bible, for deeper engagement with the faith."
         />
       </head>
-      <body>{children}</body>
+      <body>
+        <Toaster position="top-right" />
+        <NextAuthProvider>
+          <TanstackQueryProvider>{children}</TanstackQueryProvider>
+        </NextAuthProvider>
+        <BootstrapClient />
+      </body>
     </html>
   );
 }
