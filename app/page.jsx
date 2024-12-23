1b27e00772a0e2b34ba26ecda01a0b1c3e817f4f
dalienst <oduordalienst@gmail.com>
2024-12-23 08:59:49 +0300

    modify page

--- app/page.jsx
+++ app/page.jsx
@@ -5,13 +5,13 @@ import React from "react";
 export default function LandingPage() {
   return (
     <div className="container d-flex flex-column align-items-center justify-content-center min-vh-100 bg-light p-4">
-      <h1 className="hero-text mb-4">Welcome to Legio Connect</h1>
+      <h1 className="hero-text mb-4">Welcome to LegioConnect</h1>
       <p className="text-center text-muted mb-4">
         Connect, collaborate, and engage with your community like never before.
       </p>
-      <Link className="btn hero-btn btn-lg" href="/auth/login">
+      {/* <Link className="btn hero-btn btn-lg" href="/auth/login">
         Get Started
-      </Link>
+      </Link> */}
     </div>
   );
 }
