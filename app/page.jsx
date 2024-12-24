5aa6f5a0e04d06261136cd3e02f0e31908f3a5b4
dalienst <oduordalienst@gmail.com>
2024-12-25 00:19:37 +0300

    complete landing page

--- app/page.jsx
+++ app/page.jsx
@@ -10,8 +10,8 @@ export default function LandingPage() {
     <>
       <Navbar />
       <div className="container py-5">
-        <section className="mb-3">
-          <h1 className="text-center fw-bold">LegioConnect Application</h1>
+        <section className="mb-5">
+          <h1 className="text-center fw-bold" style={{ color: "#4b1719" }}>LegioConnect Application</h1>
           <p className="text-center w-75 mx-auto text-muted lead">
             LegioConnect is an application created for members of Legio Maria
             and those interested in learning more about the faith. It offers
@@ -43,6 +43,36 @@ export default function LandingPage() {
               width={1000}
               height={500}
             />
+          </div>
+        </section>
+
+        <section className="mb-5 py-5">
+          <div className="row">
+            <div className="col-md-6 col-sm-12 mb-3">
+              <h3>Get Started with LegioConnect!</h3>
+              <p>Connect . Grow . Learn</p>
+              <p>
+                LegioConnect is an application created for members of Legio
+                Maria and those interested in learning more about the faith. It
+                offers rich content on Legio Maria’s history, prayers, and the
+                Bible, for deeper engagement with the faith.
+              </p>
+            </div>
+            <div className="col-md-6 col-sm-12">
+              {appFeatures?.map((feature) => (
+                <div key={feature.id} className="mb-3 card p-3">
+                  <div className="d-flex gap-3 align-items-start">
+                    {feature?.icon}
+                    <div>
+                      <h6 className="fw-semibold feature-title mb-0">
+                        {feature?.title}
+                      </h6>
+                      <p className="text-muted">{feature?.description}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </section>
       </div>
