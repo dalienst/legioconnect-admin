c76124d6aa32a280a33fd72f5665248b81f65cc0
dalienst <oduordalienst@gmail.com>
2024-12-24 22:46:13 +0300

    added screenshots

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 import Navbar from "@/components/landing/Navbar";
+import { appFeatures } from "@/data/features";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -8,7 +10,7 @@ export default function LandingPage() {
     <>
       <div className="container py-3">
         <Navbar />
-        <section className="mt-5">
+        <section className="mt-5 mb-5">
           <h1 className="text-center">Welcome to LegioConnect</h1>
           <p className="text-center lead">
             LegioConnect is an application created for members of Legio Maria
@@ -16,6 +18,32 @@ export default function LandingPage() {
             rich content on Legio Maria’s history, prayers, and the Bible, for
             deeper engagement with the faith.
           </p>
+        </section>
+
+        <section className="mb-5">
+          <h2 className="text-center">Features</h2>
+          <div className="row">
+            {appFeatures.map((feature) => (
+              <div className="col-md-4 col-sm-12" key={feature.id}>
+                <div className="card h-100">
+                  <div className="card-body">
+                    {feature.icon}
+                    <h5 className="card-title">{feature.title}</h5>
+                    <p className="card-text">{feature.description}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+
+        <section className="mb-5">
+          <h2 className="text-center">Get Started</h2>
+          <div className="row">
+            <div className="col-sm-12 col-md-6">
+              <Image src="/screens.png" alt="hero" width={500} height={500} />
+            </div>
+          </div>
         </section>
       </div>
     </>
---
+++ data/features.jsx
@@ -0,0 +1,22 @@
+const appFeatures = [
+  {
+    id: 1,
+    icon: <i className="bi bi-book-half icon-color"></i>,
+    title: "Bible",
+    description: "Read the Holy Scriptures.",
+  },
+  {
+    id: 2,
+    icon: <i className="bi bi-book-fill icon-color"></i>,
+    title: "KitapLemo",
+    description: "Legio Maria prayer books.",
+  },
+  {
+    id: 3,
+    icon: <i className="bi bi-heart icon-color"></i>,
+    title: "Highlights",
+    description: "Highlight your favorite passages.",
+  },
+];
+
+export { appFeatures };
