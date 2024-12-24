185a6f59b322b7c029cedd37a4aa0c12d8cc1663
dalienst <oduordalienst@gmail.com>
2024-12-24 23:51:49 +0300

    added images

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,42 @@ export default function LandingPage() {
   return (
     <>
       <Navbar />
-      <div className="container-fluid py-5"></div>
+      <div className="container py-5">
+        <section className="mb-3">
+          <h1 className="text-center fw-bold">LegioConnect Application</h1>
+          <p className="text-center w-75 mx-auto text-muted lead">
+            LegioConnect is an application created for members of Legio Maria
+            and those interested in learning more about the faith. It offers
+            rich content on Legio Maria’s history, prayers, and the Bible, for
+            deeper engagement with the faith.
+          </p>
+          <div className="text-center">
+            <Link
+              href=""
+              className="d-inline-flex align-items-center gap-2"
+              style={{ width: "auto" }}
+            >
+              <Image
+                src="/getitongoogle.png"
+                alt="playstore"
+                width={100}
+                height={100}
+                className="img-fluid"
+              />
+            </Link>
+          </div>
+
+          <div className="mt-3 text-center">
+            <Image
+              src="/spaced.png"
+              alt="land"
+              className="img-fluid"
+              width={1000}
+              height={500}
+            />
+          </div>
+        </section>
+      </div>
     </>
   );
 }
