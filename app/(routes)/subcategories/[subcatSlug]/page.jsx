45a6c17dde49664ffe24c16a46c00d0c26d5fa62
dalienst <oduordalienst@gmail.com>
2025-01-24 23:52:08 +0300

    display prayers

--- app/(routes)/subcategories/[subcatSlug]/page.jsx
+++ app/(routes)/subcategories/[subcatSlug]/page.jsx
@@ -94,7 +94,26 @@ function SubcategoryDetail({ params }) {
         </div>
       </section>
 
-      <section className="mb-3 row"></section>
+      <section className="mb-3">
+        <h6>Prayers</h6>
+        {subcategory?.prayers && subcategory?.prayers?.length > 0 ? (
+          <>
+            {subcategory?.prayers?.map((prayer) => (
+              <div className="card mb-3" key={prayer?.id}>
+                <div className="card-body">
+                  <h5 className="card-title">{prayer?.title}</h5>
+                  <p className="card-text">{prayer?.content}</p>
+                </div>
+              </div>
+            ))}
+          </>
+        ) : (
+          <div className="alert alert-info">
+            <i className="bi bi-info-circle me-2"></i>
+            No prayers found
+          </div>
+        )}
+      </section>
 
       {/* modal to update subcategory */}
       <Modal
