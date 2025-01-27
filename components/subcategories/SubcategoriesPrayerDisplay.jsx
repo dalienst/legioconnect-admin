db7364f700161c0d35178d6f99d7fa6bc1881ca8
dalienst <oduordalienst@gmail.com>
2025-01-27 09:23:45 +0300

    add alert info

--- components/subcategories/SubcategoriesPrayerDisplay.jsx
+++ components/subcategories/SubcategoriesPrayerDisplay.jsx
@@ -2,10 +2,12 @@
 import UpdatePrayer from "@/forms/prayers/UpdatePrayer";
 import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
+import Toast from "react-bootstrap/Toast";
 
 function SubcategoriesPrayerDisplay({ subcategory, refetchSubcategory }) {
   const [show, setShow] = useState(false);
   const [selectedPrayer, setSelectedPrayer] = useState(null);
+  const [open, setOpen] = useState(false);
 
   const handleShow = (prayer) => {
     setSelectedPrayer(prayer);
@@ -19,7 +21,28 @@ function SubcategoriesPrayerDisplay({ subcategory, refetchSubcategory }) {
 
   return (
     <>
-      <h6>Prayers</h6>
+      <h5 className="fw-semibold mb-3">Prayers</h5>
+
+      <Toast className="w-100 mb-3 toast">
+        <div className="toast-header">
+          <strong className="me-auto">
+            <i className="bi bi-info-circle me-2"></i>Info
+          </strong>
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="toast"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div className="toast-body">
+          To update the prayer&apos;s position, click the &quot;Update
+          Prayer&quot; button and adjust the position. Prayers sharing the same
+          position are ordered by creation time, with the earliest appearing
+          first.
+        </div>
+      </Toast>
+
       {subcategory?.prayers && subcategory?.prayers?.length > 0 ? (
         <>
           {subcategory?.prayers?.map((prayer) => (
