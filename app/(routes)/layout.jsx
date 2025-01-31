2291b0e5bf2cb75851c48d124282a6ec866af2c4
dalienst <oduordalienst@gmail.com>
2025-01-31 12:13:31 +0300

    start working on sidebar

--- app/(routes)/layout.jsx
+++ app/(routes)/layout.jsx
@@ -1,12 +1,24 @@
 "use client";
 import Navbar from "@/components/dashboard/Navbar";
+import Sidebar from "@/components/dashboard/Sidebar";
 import React from "react";
 
 export default function MainLayout({ children }) {
   return (
-    <div>
-      <Navbar />
-      {children}
+    <div className="container-fluid vh-100">
+      <div className="row h-100">
+        <div className="col-lg-2 d-none d-lg-block bg-light border-end">
+          <Sidebar />
+        </div>
+
+        {/* main content */}
+        <div className="col-lg-10 col-sm-12">
+          <div className="container-fluid py-2">
+            <Navbar />
+            {children}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ components/dashboard/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="d-flex flex-column justify-content-between h-100"></div>
+  );
+}
+
+export default Sidebar;
