dbe5478c8cbf2a12215a67a5150d5c1aaf4d3970
dalienst <oduordalienst@gmail.com>
2025-02-11 21:07:12 +0300

    add button

--- app/(routes)/dailymass/page.jsx
+++ app/(routes)/dailymass/page.jsx
@@ -1,7 +1,45 @@
+"use client";
+import LoadingSpinner from "@/components/general/LoadingSpinner";
+import { useFetchDailyMass } from "@/hooks/dailymass/actions";
+import Link from "next/link";
 import React from "react";
 
 function DailyMass() {
-  return <div>DailyMass</div>;
+  const {
+    isLoading: isLoadingDailyMass,
+    data: dailyMass,
+    refetch: refetchDailyMass,
+  } = useFetchDailyMass();
+
+  if (isLoadingDailyMass) return <LoadingSpinner />;
+
+  return (
+    <div className="container-fluid">
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link href="/dashboard">Dashboard</Link>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            Daily Mass
+          </li>
+        </ol>
+      </nav>
+
+      <section className="mb-3 d-flex flex-row flex-md-row justify-content-between align-items-start align-items-md-center">
+        <div>
+          <h3 className="dash-text">Daily Mass</h3>
+          <p className="lead small">Review and handle daily mass here.</p>
+        </div>
+
+        <div>
+          <button className="btn btn-connect btn-sm">Create</button>
+        </div>
+      </section>
+
+      <section className="mb-3"></section>
+    </div>
+  );
 }
 
 export default DailyMass;
