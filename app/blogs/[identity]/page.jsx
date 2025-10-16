75ad63922746eebef96f39b18414a5df5282610e
Dalienst Owino <oduordalienst@gmail.com>
2025-10-16 10:56:53 +0300

    display content

---
+++ app/blogs/[identity]/page.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Navbar from "@/components/landing/Navbar";
+import LoadingSpinner from "@/components/general/LoadingSpinner";
+import { useFetchTeachingDetail } from "@/hooks/teachings/actions";
+import { useParams } from "next/navigation";
+import React from "react";
+import Footer from "@/components/landing/Footer";
+
+function BlogIdentity() {
+  const { identity } = useParams();
+
+  const { isLoading: isLoadingBlogDetail, data: blog } =
+    useFetchTeachingDetail(identity);
+
+  if (isLoadingBlogDetail) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div className="container py-4">
+        {/* header */}
+        <section className="mb-3">
+          <h1 className="text-center fw-bold">{blog?.title}</h1>
+
+          <p className="text-center w-75 mx-auto text-muted lead">
+            {blog?.location} - {blog?.date}
+          </p>
+        </section>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default BlogIdentity;
